eafac320cc18a19331ad39e048bf64aff40993b2
rochamanuela <rochadesouzamanuela@gmail.com>
2024-12-20 15:29:37 -0300

    feat: add modal

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import IconFigma from '../../assets/vectors/icon-figma.svg'
 import IconGithub from '../../assets/vectors/icon-github.svg'
 
-export default function Card({data}) {
+export default function Card({ data }) {
+
+    const [modalDetails, setModalDetails] = useState(false)
 
     const truncateText = (text, maxLength) => {
         if (text.length > maxLength) {
@@ -12,26 +14,41 @@ export default function Card({data}) {
     }
 
     return (
-        <div className='font-poppins w-80 rounded-xl border-gray-300 border'>
-            <div className='image rounded-t-xl bg-cover h-48 flex flex-row text-white justify-between text-xxs px-6 py-5' style={{ backgroundImage: `linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 100%), url(${data.image})` }}>
-                <p>{data.period}</p>
-                <p>{data.lecturer}</p>
-            </div>
-            <div className='p-6'>
-                <p className='font-sora font-semibold text-xl'>{data.projectName}</p>
-                <p className='mt-3 text-xs'>{truncateText(data.projectDescription, 116)}</p>
-                <div className='flex flex-row justify-between items-center mt-5'>
-                    <div className='flex flex-row gap-1'>
-                        <a className='w-8 h-8 flex items-center justify-center rounded-3xl transition-all duration-300 cursor-pointer hover:bg-slate-200' href={data.figmaLink}>
-                            <img src={IconFigma} />
-                        </a>
-                        <a className='w-8 h-8 flex items-center justify-center rounded-3xl transition-all duration-300 cursor-pointer hover:bg-slate-200' href={data.githubLink}>
-                            <img src={IconGithub} />
-                        </a>
+        <>
+            <div className='font-poppins w-80 rounded-xl border-gray-300 border'>
+                <div className='image rounded-t-xl bg-cover h-48 flex flex-row text-white justify-between text-xxs px-6 py-5' style={{ backgroundImage: `linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 100%), url(${data.image})` }}>
+                    <p>{data.period}</p>
+                    <p>{data.lecturer}</p>
+                </div>
+                <div className='p-6'>
+                    <p className='font-sora font-semibold text-xl'>{data.projectName}</p>
+                    <p className='mt-3 text-xs'>{truncateText(data.projectDescription, 116)}</p>
+                    <div className='flex flex-row justify-between items-center mt-5'>
+                        <div className='flex flex-row gap-1'>
+                            <a className='w-8 h-8 flex items-center justify-center rounded-3xl transition-all duration-300 cursor-pointer hover:bg-slate-200' href={data.figmaLink}>
+                                <img src={IconFigma} />
+                            </a>
+                            <a className='w-8 h-8 flex items-center justify-center rounded-3xl transition-all duration-300 cursor-pointer hover:bg-slate-200' href={data.githubLink}>
+                                <img src={IconGithub} />
+                            </a>
+                        </div>
+                        <div
+                            className='text-xs font-medium cursor-pointer px-4 py-2 rounded-md transition-all duration-300 hover:bg-slate-200'
+                            onClick={() => setModalDetails(true)}>
+                            Mais detalhes
+                        </div>
                     </div>
-                    <div className='text-xs font-medium cursor-pointer px-4 py-2 rounded-md transition-all duration-300 hover:bg-slate-200'>Mais detalhes</div>
                 </div>
             </div>
-        </div>
+
+            {modalDetails && (
+                <div className='fixed bg-black bg-opacity-45 w-full h-full z-50 top-0'>
+                    <div className='absolute w-[750px] h-[550px] bg-white m-auto top-[10%] left-[20%]'>
+                        <h1>DEU CERTOOOO</h1>
+                        <button onClick={() => setModalDetails(false)}>fechar</button>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
